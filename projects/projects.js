d4d6ae2359224f57b23bf5bacf5c5f9ce85fe34e
Zachary Thornton <thorntonzachary02@gmail.com>
2024-09-22 18:32:57 -0400

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,4 @@
-const links = ["../home/home.html", "../lightningTalks/lightningTalks.html", "../maintenance/maintenance.html"];
+const links = ["../home/home.html", "../lightningTalks/lightningTalks.html", "../maintenance/maintenance.html", "https://github.com/ZacharyMThornton"];
 
 const linkButtons = document.getElementsByClassName("link");
 
